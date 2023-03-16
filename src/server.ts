1be90b42d4004a4950383e7d060aa359673e93f9
ecthon.almeida <ecthon.almeida@hitechnologies.com.br>
2023-03-16 12:58:59 -0400

    feat: Add basic routing

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,31 @@
 import express from 'express';
 import { Router, Request, Response } from 'express';
 
-const PORT = 3333;
-
 const app = express();
 const route = Router();
+
+const PORT = 3333;
 app.use(express.json());
 
-// ROTAS
+// GET
 route.get('/', (request: Request, response: Response) => {
     response.json({message: 'Hello!'});
 });
+
+// POST
+route.post('/', (request: Request, response: Response) => {
+    response.json({message: 'Get a POST request'})
+});
+
+// PUT
+route.put('/user', (request: Request, response: Response) => {
+    response.json({message: 'Got a PUT request at /user'})
+});
+
+//DELETE
+route.delete('/user', (request: Request, response: Response) => {
+    response.json({message: 'Got a DELETE request at /user'})
+  })
 
 app.use(route);
 
